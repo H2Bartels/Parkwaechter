2ac36d3275f4e48cb813fde0df7bf8828baf0a82
H2Bartels <benjamin.bartels@h2perform.de>
2025-12-16 12:18:31 +0100

    Initial Commit

---
+++ Parkwaechter/ParkwaechterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkwaechterApp.swift
+//  Parkwaechter
+//
+//  Created by Benjamin Bartels on 16.12.25.
+//
+
+import SwiftUI
+
+@main
+struct ParkwaechterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
